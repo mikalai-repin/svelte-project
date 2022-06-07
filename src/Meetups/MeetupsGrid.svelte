14ca2a05bbd3ee96885e46499e7b72f0aed5ce6f
Mikalai Repin <mikalai.repin@gmail.com>
2022-06-07 10:51:52 +0300

    Initial commit

---
+++ src/Meetups/MeetupsGrid.svelte
@@ -0,0 +1,28 @@
+<script>
+  import MeetupItem from './MeetupItem.svelte';
+
+  export let meetups;
+</script>
+
+<section class="meetups">
+  {#each meetups as meetup (meetup.id)}
+    <MeetupItem {...meetup}/>
+  {/each}
+</section>
+
+<style>
+
+  .meetups {
+      width: 100%;
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-gap: 1rem;
+  }
+
+  @media (min-width: 768px) {
+      .meetups {
+          grid-template-columns: repeat(2, 1fr);
+      }
+  }
+
+</style>
---
+++ src/UI/Header.svelte
@@ -0,0 +1,23 @@
+<header class="header">
+  <h1 class="header__title">Meetups</h1>
+</header>
+
+<style>
+    .header {
+        position: fixed;
+        width: 100%;
+        top: 0;
+        left: 0;
+        height: 4rem;
+        background: #cf0056;
+        justify-content: center;
+        display: flex;
+        align-items: center;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.26);
+    }
+    .header__title {
+        color: white;
+        font-family: 'Times New Roman', serif;
+        margin: 0;
+    }
+</style>
